152824cc127e3d99ed0a4c517fee20bc28d386a7
jamesziou <653978237@qq.com>
2021-05-27 21:40:35 +0800

    eureka 追加

--- config-service/src/main/java/com/zhaojn/config/ConfigServiceApplication.java
+++ config-service/src/main/java/com/zhaojn/config/ConfigServiceApplication.java
@@ -15,6 +15,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 配置服务启动类<br>
@@ -28,6 +29,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @ServletComponentScan
 @EnableConfigServer
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigServiceApplication {
   public static void main(String[] args) {
     SpringApplication.run(ConfigServiceApplication.class, args);
---
+++ eureka-server/src/main/java/com/zhaojn/eureka/EurekaServerApplication.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C), 2015-2021, zhaojn
+ * FileName: Application
+ * Author:   zhaojn
+ * Date:     2021/5/27 20:11
+ * Description: 启动类
+ * History:
+ * 作者姓名          修改时间          版本号          描述
+ * zhaojn           2021/5/27 20:11   1.0.0           初版
+ */
+
+package com.zhaojn.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 启动类<br>
+ *
+ * @author zhaojn
+ * @create 2021/5/27
+ * @since 1.0.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaServerApplication.class, args);
+  }
+}
